db8698fcc9d13904de70e1e77e7d22ec8aab6b24
PavanSai1729 <pavanmaths1729@gmail.com>
2024-05-30 01:06:07 +0530

    second commit

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require("../models/productmodel.js");
 
 exports.getAddproduct = (req, res, next) => {
     res.render('add-product', {
@@ -11,12 +11,14 @@ exports.getAddproduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
   };
 
 
  exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
---
+++ models/productmodel.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+
+const products =[];
+
+module.exports = class Product {
+    constructor(title){
+        this.title = title;
+    }
+
+    save(){
+        const p = path.join(path.dirname(process.mainModule.filename),
+        "data",
+        "products.json"
+    );
+
+    fs.readFile(p, (err, fileContent)=>{
+        let products = [];
+        if(!err){
+            products = JSON.parse(fileContent);
+        }
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err)=>{
+            if(err){
+            console.log(err);
+            }
+        });
+    });
+
+}
+
+    static fetchAll(){
+        fs.readFile(p,(err, fileContent) => {
+            if(err){
+                return [];
+            }
+            return JSON.parse(fileContent);
+        })
+        
+    }
+
+
+};
