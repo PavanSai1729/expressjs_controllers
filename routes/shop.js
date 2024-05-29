b8c90bd22d85aea891688435c65322f04ca0e1f8
PavanSai1729 <pavanmaths1729@gmail.com>
2024-05-29 23:52:11 +0530

    first commit

---
+++ routes/shop.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require("../controllers/product.js");
+
+const router = express.Router();
+
+router.get('/', productsController.getProducts);
+
+module.exports = router;
+
+
